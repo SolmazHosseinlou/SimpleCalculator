6ce13c1d07f20dd2292f306dafd7fd655163b096
HSol <59023050+SolmazHosseinlou@users.noreply.github.com>
2020-02-14 12:02:05 +0330

    Add files via upload
    اینترفیس و کلاس برای محاسبه محیط و مساحت مستطیل

---
+++ IRectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimeter_Area
+{
+    interface IRectangle
+    {
+        uint Perimeter(uint length, uint width);
+        uint Area(uint length, uint width);
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perimeter_Area
+{
+    class Rectangle : IRectangle
+    {
+        public uint Area(uint length, uint width)
+        {
+            return length * width;
+        }
+
+        public uint Perimeter(uint length, uint width)
+        {
+            return (length + width) * 2;
+        }
+    }
+}
